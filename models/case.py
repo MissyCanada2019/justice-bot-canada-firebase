0fa0ad132ffd8dcf77fa152e32b816139667ee1f
terri <smarftdisputecanada@gmail.com>
2025-08-14 17:38:26 -0400

    Set up Flask application with SQLAlchemy and register blueprints

---
+++ models/case.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from utils.db import db
+
+class Case(db.Model):
+    __tablename__ = "case"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, index=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
